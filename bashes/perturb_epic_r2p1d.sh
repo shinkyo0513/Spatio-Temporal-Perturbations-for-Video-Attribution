4572c3e9649f4d0bd3d3775de30e953e2eed6657
acb11711tx <acb11711tx@es1.abci.local>
2020-08-24 22:29:02 +0900

    aa

--- bashes/perturb_epic_r2p1d.sh
+++ bashes/perturb_epic_r2p1d.sh
@@ -2,7 +2,7 @@
 
 #$ -N epic_r2p1d
 #$ -l rt_F=1
-#$ -l h_rt=72:00:00
+#$ -l h_rt=9:00:00
 #$ -o outs_epic/r2p1d_perturb_core_proc
 #$ -j y
 #$ -cwd
@@ -12,10 +12,4 @@ source ${HOME}/.bashrc
 conda activate pytorch1.1
 
 python run_all.py --dataset epic --model r2p1d --only_test --vis_method perturb \
---perturb_niter 1200 --perturb_withcore --perturb_num_keyframe 11 --batch_size 4
-
-python run_all.py --dataset epic --model r2p1d --only_test --vis_method perturb \
---perturb_niter 1200 --perturb_withcore --perturb_num_keyframe 7 --batch_size 4
-
-python run_all.py --dataset epic --model r2p1d --only_test --vis_method perturb \
---perturb_niter 1200 --perturb_withcore --perturb_num_keyframe 3 --batch_size 4
+--perturb_niter 1200 --perturb_withcore --perturb_num_keyframe 5 --batch_size 4
