06e5d94faac6b3edff5197e22fd62d3931e94218
acb11711tx <acb11711tx@es1.abci.local>
2020-12-29 23:31:33 +0900

    changed retrain ucf101_24 at abci.

--- bashes/kar_ucf101_r50l_grad_cam.sh
+++ bashes/kar_ucf101_r50l_grad_cam.sh
@@ -2,7 +2,7 @@
 
 #$ -N ucf_r50l
 #$ -l rt_F=1
-#$ -l h_rt=10:00:00
+#$ -l h_rt=20:00:00
 #$ -o outs_ucf101/r50l_kar_grad_cam.txt
 #$ -j y
 #$ -cwd
@@ -12,31 +12,31 @@ source ${HOME}/.bashrc
 conda activate pytorch1.1
 
 python model_train/retrain_ucf101_24_model.py --model r50l --long_range \
---testlist_idx 1 --multi_gpu --num_epochs 40 \
+--testlist_idx 1 --multi_gpu --num_epochs 80 \
 --vis_method grad_cam \
 --perturb_ratio 0.05 --perturb_mode keep
 
 python model_train/retrain_ucf101_24_model.py --model r50l --long_range \
---testlist_idx 1 --multi_gpu --num_epochs 40 \
+--testlist_idx 1 --multi_gpu --num_epochs 80 \
 --vis_method grad_cam \
 --perturb_ratio 0.1 --perturb_mode keep
 
 python model_train/retrain_ucf101_24_model.py --model r50l --long_range \
---testlist_idx 1 --multi_gpu --num_epochs 40 \
+--testlist_idx 1 --multi_gpu --num_epochs 80 \
 --vis_method grad_cam \
 --perturb_ratio 0.3 --perturb_mode keep
 
 python model_train/retrain_ucf101_24_model.py --model r50l --long_range \
---testlist_idx 1 --multi_gpu --num_epochs 40 \
+--testlist_idx 1 --multi_gpu --num_epochs 80 \
 --vis_method grad_cam \
 --perturb_ratio 0.5 --perturb_mode keep
 
 python model_train/retrain_ucf101_24_model.py --model r50l --long_range \
---testlist_idx 1 --multi_gpu --num_epochs 40 \
+--testlist_idx 1 --multi_gpu --num_epochs 80 \
 --vis_method grad_cam \
 --perturb_ratio 0.7 --perturb_mode keep
 
 python model_train/retrain_ucf101_24_model.py --model r50l --long_range \
---testlist_idx 1 --multi_gpu --num_epochs 40 \
+--testlist_idx 1 --multi_gpu --num_epochs 80 \
 --vis_method grad_cam \
 --perturb_ratio 0.9 --perturb_mode keep
